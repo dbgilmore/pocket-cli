9c13e9833c6ef6866ec214321cc0195f2e17ad16
David Gilmore <dgilmore@spotxchange.com>
2017-05-16 23:49:36 +0100

    Bit more PEP8

--- pocket_time_tagger.py
+++ pocket_time_tagger.py
@@ -31,7 +31,6 @@ class PocketTimeTagger:
         return int(pocket_constants.iRoundBase *
                    round(float(iNumber) / pocket_constants.iRoundBase))
 
-
     def tagUntaggedItems(self):
         """
         Do the thing.
